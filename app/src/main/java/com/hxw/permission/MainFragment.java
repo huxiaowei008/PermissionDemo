810678a7eb2f72edc2973437f70ef3123f5e144a
huxiaowei <huxiaowei008@126.com>
2018-04-28 15:52:19 +0800

    Initial commit

---
+++ app/src/main/java/com/hxw/permission/MainFragment.java
@@ -0,0 +1,9 @@
+package com.hxw.permission;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author hxw on 2018/4/27.
+ */
+public class MainFragment extends Fragment {
+}
---
+++ app/src/main/java/com/hxw/permission/PermissionUtils.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * @author hxw on 2018/4/28.
+ */
+public class PermissionUtils {
+}
